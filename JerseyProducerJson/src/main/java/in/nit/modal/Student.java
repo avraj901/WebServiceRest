aa9910c00d2b701e44e0e60da37e36c3ad6c7573
avraj901 <avraj901@gmail.com>
2020-07-17 16:27:01 -0400

    prodece json data program

---
+++ JerseyProducerJson/src/main/java/in/nit/modal/Student.java
@@ -0,0 +1,53 @@
+package in.nit.modal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+	private Integer stdentId;
+	private String studentName;
+	private Double studentFee;
+	public Integer getStdentId() {
+		return stdentId;
+	}
+	public void setStdentId(Integer stdentId) {
+		this.stdentId = stdentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public Double getStudentFee() {
+		return studentFee;
+	}
+	public void setStudentFee(Double studentFee) {
+		this.studentFee = studentFee;
+	}
+	@Override
+	public String toString() {
+		return "Student [stdentId=" + stdentId + ", studentName=" + studentName + ", studentFee=" + studentFee + "]";
+	}
+	public Student(Integer stdentId, String studentName, Double studentFee) {
+		super();
+		this.stdentId = stdentId;
+		this.studentName = studentName;
+		this.studentFee = studentFee;
+	}
+	public Student() {
+		super();
+	}
+	
+	
+	
+}
